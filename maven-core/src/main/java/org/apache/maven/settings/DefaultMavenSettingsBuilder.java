9372f8e7a78e82646c9822a61a3ead5e4ef82f8e
Jason van Zyl <jvanzyl@apache.org>
2007-09-07 05:25:49 +0000

    o profiles are now decoupled from settings. so that i don't do this is one huge commit i need to move settings stuff into the core   while i transform the settings components to take a MavenSession. Any component executing within Maven should be able to use   a session. the session will contain everything required and any new component added to the system should only take the   session as a parameter. same pattern for all components. that's the goal. it will take a few hops.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@573462 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core/src/main/java/org/apache/maven/settings/DefaultMavenSettingsBuilder.java
@@ -0,0 +1,227 @@
+package org.apache.maven.settings;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.context.BuildContextManager;
+import org.apache.maven.context.SystemBuildContext;
+import org.apache.maven.settings.io.xpp3.SettingsXpp3Reader;
+import org.apache.maven.settings.io.xpp3.SettingsXpp3Writer;
+import org.apache.maven.settings.validation.SettingsValidationResult;
+import org.apache.maven.settings.validation.SettingsValidator;
+import org.codehaus.plexus.logging.AbstractLogEnabled;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.interpolation.EnvarBasedValueSource;
+import org.codehaus.plexus.util.interpolation.PropertiesBasedValueSource;
+import org.codehaus.plexus.util.interpolation.RegexBasedInterpolator;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author jdcasey
+ * @version $Id$
+ */
+public class DefaultMavenSettingsBuilder
+    extends AbstractLogEnabled
+    implements MavenSettingsBuilder
+{
+    private SettingsValidator validator;
+
+    private BuildContextManager manager;
+
+    /**
+     * @since 2.1
+     */
+    public Settings buildSettings( File userSettingsFile, File globalSettingsFile )
+        throws IOException, XmlPullParserException
+    {
+        if ( ( globalSettingsFile == null ) && ( userSettingsFile == null ) )
+        {
+            getLogger().debug(
+                               "No settings files provided, and default locations are disabled for this request. Returning empty Settings instance." );
+            return new Settings();
+        }
+
+        getLogger().debug( "Reading global settings from: " + globalSettingsFile );
+
+        Settings globalSettings = readSettings( globalSettingsFile );
+
+        if ( globalSettings == null )
+        {
+            globalSettings = new Settings();
+        }
+
+        getLogger().debug( "Reading user settings from: " + userSettingsFile );
+
+        Settings userSettings = readSettings( userSettingsFile );
+
+        if ( userSettings == null )
+        {
+            userSettings = new Settings();
+        }
+
+        validateSettings( globalSettings, globalSettingsFile );
+
+        validateSettings( userSettings, userSettingsFile );
+
+        SettingsUtils.merge( userSettings, globalSettings, TrackableBase.GLOBAL_LEVEL );
+
+        activateDefaultProfiles( userSettings );
+
+        userSettings = interpolate( userSettings );
+
+        return userSettings;
+    }
+
+    private Settings interpolate( Settings settings )
+        throws IOException
+    {
+        List activeProfiles = settings.getActiveProfiles();
+
+        StringWriter writer = new StringWriter();
+        new SettingsXpp3Writer().write( writer, settings );
+
+        String serializedSettings = writer.toString();
+
+        SystemBuildContext sysContext = SystemBuildContext.getSystemBuildContext( manager, true );
+
+        RegexBasedInterpolator interpolator = new RegexBasedInterpolator();
+
+        interpolator.addValueSource( new PropertiesBasedValueSource( sysContext.getSystemProperties() ) );
+        interpolator.addValueSource( new EnvarBasedValueSource() );
+
+        serializedSettings = interpolator.interpolate( serializedSettings, "settings" );
+
+        Settings result;
+        try
+        {
+            result = new SettingsXpp3Reader().read( new StringReader( serializedSettings ) );
+
+            result.setActiveProfiles( activeProfiles );
+        }
+        catch ( XmlPullParserException e )
+        {
+            IOException error = new IOException( "Failed to parse interpolated settings: " + e.getMessage() );
+            error.initCause( e );
+
+            throw error;
+        }
+
+        return result;
+    }
+
+    private Settings readSettings( File settingsFile )
+        throws IOException, XmlPullParserException
+    {
+        if ( settingsFile == null )
+        {
+            getLogger().debug( "Settings file is null. Returning." );
+            return null;
+        }
+
+        Settings settings = null;
+
+        if ( settingsFile.exists() && settingsFile.isFile() )
+        {
+            getLogger().debug( "Settings file is a proper file. Reading." );
+
+            FileReader reader = null;
+            try
+            {
+                reader = new FileReader( settingsFile );
+
+                SettingsXpp3Reader modelReader = new SettingsXpp3Reader();
+
+                settings = modelReader.read( reader );
+
+                RuntimeInfo rtInfo = new RuntimeInfo( settings );
+
+                rtInfo.addLocation( settingsFile.getAbsolutePath() );
+
+                settings.setRuntimeInfo( rtInfo );
+            }
+            catch ( XmlPullParserException e )
+            {
+                getLogger().error( "Failed to read settings from: " + settingsFile + ". Throwing XmlPullParserException..." );
+
+                throw e;
+            }
+            catch ( IOException e )
+            {
+                getLogger().error( "Failed to read settings from: " + settingsFile + ". Throwing IOException..." );
+
+                throw e;
+            }
+            finally
+            {
+                IOUtil.close( reader );
+            }
+        }
+
+        return settings;
+    }
+
+    private void activateDefaultProfiles( Settings settings )
+    {
+        List activeProfiles = settings.getActiveProfiles();
+
+        for ( Iterator profiles = settings.getProfiles().iterator(); profiles.hasNext(); )
+        {
+            Profile profile = (Profile) profiles.next();
+            if ( ( profile.getActivation() != null ) && profile.getActivation().isActiveByDefault()
+                && !activeProfiles.contains( profile.getId() ) )
+            {
+                settings.addActiveProfile( profile.getId() );
+            }
+        }
+    }
+
+    private void validateSettings( Settings settings, File location )
+        throws IOException
+    {
+        SettingsValidationResult validationResult = validator.validate( settings );
+
+        if ( validationResult.getMessageCount() > 0 )
+        {
+            throw new IOException( "Failed to validate Settings file at " + location + "\n" + validationResult.render( "\n" ) );
+        }
+    }
+
+    /**
+      * @return a <code>Settings</code> object from the user and global settings file.
+      * @throws IOException if any
+      * @throws XmlPullParserException if any
+      * @deprecated Use {@link org.apache.maven.settings.MavenSettingsBuilder#buildSettings(java.io.File,java.io.File)} instead.
+     */
+    public Settings buildSettings()
+        throws IOException, XmlPullParserException
+    {
+        String mavenHome = System.getProperty( "maven.home" );
+        String userHome = System.getProperty( "user.home" );
+
+        return buildSettings( new File( userHome, ".m2/settings.xml" ), new File( mavenHome, "conf/settings.xml" ) );
+    }
+}
---
+++ maven-core/src/main/java/org/apache/maven/settings/MavenSettingsBuilder.java
@@ -0,0 +1,61 @@
+package org.apache.maven.settings;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Builder for the user or global settings. By default, the settings files are located:
+ * <ul>
+ * <li>user settings: ${user.home}/settings.xml</li>
+ * <li>global settings: ${maven.home}/conf/settings.xml</li>
+ * </ul>
+ *
+ * @author jdcasey
+ * @version $Id$
+ */
+public interface MavenSettingsBuilder
+{
+    String ROLE = MavenSettingsBuilder.class.getName();
+
+    /**
+    * @return a <code>Settings</code> object from the user and global settings file.
+    * @throws IOException if any
+    * @throws XmlPullParserException if any
+    * @deprecated Use {@link org.apache.maven.settings.MavenSettingsBuilder#buildSettings(java.io.File,java.io.File)} instead.
+    */
+   Settings buildSettings()
+       throws IOException, XmlPullParserException;
+
+    /**
+     *
+     * @param userSettingsFile
+     * @param globalSettingsFile
+     * @return a <code>Settings</code> object from the user and global settings file.
+     * @throws IOException if any
+     * @throws XmlPullParserException if any
+     * @since 2.1
+     */
+    Settings buildSettings( File userSettingsFile, File globalSettingsFile )
+        throws IOException, XmlPullParserException;
+}
